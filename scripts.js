974c553cd35c5dc13494b88ae621a57c4cdf351a
Qasura75 <112414285+Qasura75@users.noreply.github.com>
2023-04-08 15:08:40 +0100

    Added drawing function when hovering over the grid boxes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 const mainGrid = document.querySelector("#main-grid");
 
 
+createGrid(16,16);
 
-
-//Creates Grid
-for (i=0; i<16; i++) {
+function createGrid(maxHeight, maxWidth) {
+for (i=0; i<maxHeight; i++) {
     const gridColumn = document.createElement("div");
 
-    for (j=0; j<16; j++) {
+    for (j=0; j<maxWidth; j++) {
         const gridBox = document.createElement("div");
         gridBox.classList.add("grid-box");
         gridColumn.appendChild(gridBox);
@@ -16,4 +16,14 @@ for (i=0; i<16; i++) {
     mainGrid.appendChild(gridColumn)
 
 }
+}
+
+
+
+
+mainGrid.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("grid-box")) {
+      event.target.classList.add("grid-box-black");
+    }
+  });
 
