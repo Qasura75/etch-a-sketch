813bbfda4902e81d399a30ddb9864ab024046c2e
Qasura75 <112414285+Qasura75@users.noreply.github.com>
2023-04-16 18:16:53 +0100

    Added rainbow pen

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 const mainGrid = document.querySelector("#main-grid");
 let gridSize = 16;
+let drawingColor = "black"
+let colorChoice;
 
 createGrid(16);
+drawColor();
 
 //Creates Grid by taking size and adding rows and columns
 function createGrid(gridSize) {
   mainGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   mainGrid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-  
   let numDivs = gridSize * gridSize;
 
   for(let i = 0; i < numDivs; i++) {
@@ -17,6 +19,7 @@ function createGrid(gridSize) {
   }
 }
 
+//Deletes Drawing inside Grid and resets the size to gridSize
 function deleteDrawing() {
     mainGrid.innerHTML = "";
     createGrid(gridSize);
@@ -36,9 +39,22 @@ function resizeGrid() {
   }
 }
 
-mainGrid.addEventListener("mouseover", (event) => {
-    if (event.target.classList.contains("grid-box")) {
-      event.target.classList.add("grid-box-black");
-    }
-  });
-
+//Draws with selected color - standard is black
+function drawColor() {
+  if (colorChoice == "rainbow") {
+    mainGrid.addEventListener("mouseover", (event) => {
+      if (event.target.classList.contains("grid-box")) {
+        let drawingColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        event.target.style.backgroundColor = drawingColor;
+      }
+    });
+  }
+  else {
+    mainGrid.addEventListener("mouseover", (event) => {
+      if (event.target.classList.contains("grid-box")) {
+        drawingColor = "black";
+        event.target.style.backgroundColor = drawingColor;
+      }
+    });
+  }
+} 
